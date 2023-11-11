306e9c499f3bd6ed1eb9af43da6ef174c530d0a5
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-11-11 16:32:45 +0900

    [Add]Members

---
+++ Source/BlackOpsLegend/Members/Saruwatari/SaruwatariTestActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Members/Saruwatari/SaruwatariTestActor.h"
+
+// Sets default values
+ASaruwatariTestActor::ASaruwatariTestActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASaruwatariTestActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASaruwatariTestActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/BlackOpsLegend/Members/Usugi/UsugiTestActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Members/Usugi/UsugiTestActor.h"
+
+// Sets default values
+AUsugiTestActor::AUsugiTestActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AUsugiTestActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AUsugiTestActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
